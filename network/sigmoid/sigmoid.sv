433419413b00c681ac8da5c28cce311eb7bd4a65
js3g20 <js3g20@soton.ac.uk>
2023-02-04 15:24:49 +0000

    Add Offset Parameter

--- network/sigmoid/sigmoid.sv
+++ network/sigmoid/sigmoid.sv
@@ -20,6 +20,7 @@ constant constants_generator(
     .a4(a4),
     .a5(a5)
 );
+constants_generator.OFFSET = OFFSET;
 
 // generates e^-4
 exp_const const_exponential(
@@ -27,6 +28,7 @@ exp_const const_exponential(
     .n_rst(n_rst),
     .y(const_exp)
 );
+const_exponential.OFFSET = OFFSET;
 
 // generates e^-x
 exp x_exponential(
