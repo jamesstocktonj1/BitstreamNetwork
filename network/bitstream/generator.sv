d523376cf2811f29fc93b2dc2ac03a2fe9dec118
js3g20 <js3g20@soton.ac.uk>
2023-03-01 11:15:57 +0000

    Add 16-bit LFSR

--- network/bitstream/generator.sv
+++ network/bitstream/generator.sv
@@ -5,7 +5,7 @@ module generator(output logic y,
                  input int x,
                  input logic clk, n_rst);
 
-parameter SEED = 0'b11010;
+parameter SEED = 8'b11010;
 parameter LENGTH = 8;
 
 
@@ -19,6 +19,53 @@ always_ff @(posedge clk, negedge n_rst) begin
         lfsr <= SEED;
     else
         lfsr <= {lfsr[0] ^ lfsr[2] ^ lfsr[5] ^ lfsr[6], lfsr[LENGTH-1:1]};
+end
+
+endmodule
+
+
+module generator16(output logic y,
+                 input int x,
+                 input logic clk, n_rst);
+
+parameter SEED = 16'b11010;
+parameter LENGTH = 16;
+
+
+logic [LENGTH-1:0] lfsr;
+
+assign y = (lfsr[15:8] < x);
+// assign y = (lfsr[7:0] < x);
+
+
+always_ff @(posedge clk, negedge n_rst) begin
+    if(~n_rst)
+        lfsr <= SEED;
+    else
+        lfsr <= {lfsr[0] ^ lfsr[3] ^ lfsr[12] ^ lfsr[14] ^ lfsr[15], lfsr[LENGTH-1:1]};
+end
+
+endmodule
+
+
+module generator_exp(output logic y,
+                 input logic clk, n_rst);
+
+parameter SEED = 16'b11010;
+parameter LENGTH = 16;
+parameter VALUE = 1200;
+
+
+logic [LENGTH-1:0] lfsr;
+
+assign y = (lfsr < VALUE);
+
+
+always_ff @(posedge clk, negedge n_rst) begin
+    if(~n_rst)
+        lfsr <= SEED;
+    else
+        lfsr <= {lfsr[0] ^ lfsr[3] ^ lfsr[12] ^ lfsr[14] ^ lfsr[15], lfsr[LENGTH-1:1]};
 end
 
 endmodule
