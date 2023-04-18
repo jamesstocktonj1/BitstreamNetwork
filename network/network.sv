2515dca448615511c896f10f4b0017c1fa07c01b
js3g20 <js3g20@soton.ac.uk>
2023-04-18 15:01:14 +0100

    Add Perceptron Configuration

--- network/network.sv
+++ network/network.sv
@@ -1,4 +1,4 @@
-
+// Perceptron Network Configuration
 
 
 module network #(parameter INPUT_SIZE = 2, OUTPUT_SIZE = 1, SEED = 25)
@@ -6,23 +6,17 @@ module network #(parameter INPUT_SIZE = 2, OUTPUT_SIZE = 1, SEED = 25)
              input int network_input [0:INPUT_SIZE-1],
              input logic clk, n_rst, compute);
 
-parameter layer12_size = 2;
-
 // const int layer12_size = 2;
 logic [INPUT_SIZE-1:0] network_bitstream_input;
 logic [OUTPUT_SIZE-1:0] network_bitstream_output;
-logic [layer12_size-1:0] layer12_connect;
 
-
-// layer 1
-int w1 [0:layer12_size-1][0:INPUT_SIZE-1] = '{
-    '{172, 130},
-	'{118, 100}
+// single layer
+int w [0:OUTPUT_SIZE-1][0:INPUT_SIZE-1] = '{
+    '{213, 235}
 };
 
-// layer 2 neuron
-int w2 [0:OUTPUT_SIZE-1][0:layer12_size-1] = '{
-    '{144, 111}
+int b [0:OUTPUT_SIZE-1] = '{
+    81
 };
 
 generate
@@ -38,22 +32,14 @@ generate
 endgenerate
 
 
-layer #(.INPUT_SIZE(INPUT_SIZE), .NEURON_COUNT(layer12_size), .SEED(SEED)) layer1 (
+layer #(.INPUT_SIZE(INPUT_SIZE), .NEURON_COUNT(OUTPUT_SIZE), .SEED(SEED)) layer1 (
     .clk(clk),
     .n_rst(n_rst),
-    .layer_weights(w1),
+    .layer_weights(w),
+    .layer_bias(b),
     .layer_input(network_bitstream_input),
-    .layer_output(layer12_connect)
-);
-
-layer #(.INPUT_SIZE(layer12_size), .NEURON_COUNT(OUTPUT_SIZE), .SEED(SEED + (layer12_size * OUTPUT_SIZE))) layer2 (
-    .clk(clk),
-    .n_rst(n_rst),
-    .layer_weights(w2),
-    .layer_input(layer12_connect),
     .layer_output(network_bitstream_output)
 );
-
 
 generate
     genvar j;
